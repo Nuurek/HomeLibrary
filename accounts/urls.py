1edbc42d8ca0f0ebdf836685ac5b5cfb366da830
Nuurek <nurskijakub@gmail.com>
2017-06-12 22:14:07 +0200

    Moved common parts of accounts' views to account_base.html. Started implementing email confirmation.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import LoginView
+from .views import LoginView, SignUpView
 
 
 urlpatterns = [
-    url(r'^', LoginView.as_view()),
+    url(r'^$', LoginView.as_view(), name='login'),
+    url(r'^sign_up$', SignUpView.as_view(), name='sign_up'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class LoginView(TemplateView):
-    template_name = "login.html"
+    template_name = 'login.html'
+
+
+class SignUpView(TemplateView):
+    template_name = 'signup.html'
