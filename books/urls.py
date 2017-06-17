3cc6ac519c024c3de09f256de8e506acfa3d0b11
Nuurek <nurskijakub@gmail.com>
2017-06-17 23:40:14 +0200

    Increased Book description size. Prepared Book card design.

--- books/urls.py
+++ books/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     url(r'^copy/create', BookCopyCreateView.as_view(), name='book_copy_create'),
     url(r'^create', BookCreateView.as_view(), name='book_create'),
+    url(r'^preview', BookPreviewView.as_view(), name='book_preview'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, FormView
+from django.views.generic import CreateView, FormView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
@@ -15,9 +15,24 @@ class BookCopyCreateView(LoginRequiredMixin, CreateView):
 class BookCreateView(LoginRequiredMixin, FormView):
     form_class = BookForm
     template_name = 'books/book_create.html'
-    success_url = reverse_lazy('library_list')
+    success_url = reverse_lazy('book_preview')
 
     def form_valid(self, form):
-        cover_preview = BookCoverPreview(profile=self.request.user.userprofile, cover=form.cleaned_data['cover'])
+        user_profile = self.request.user.userprofile
+        BookCoverPreview.objects.filter(profile=user_profile).delete()
+        cover_preview = BookCoverPreview(profile=user_profile, cover=form.cleaned_data.pop('cover'))
         cover_preview.save()
+        self.request.session['book'] = form.cleaned_data
         return super(BookCreateView, self).form_valid(form)
+
+
+class BookPreviewView(LoginRequiredMixin, TemplateView):
+    template_name = 'books/book_preview.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BookPreviewView, self).get_context_data(**kwargs)
+        book = self.request.session['book']
+        book['cover'] = BookCoverPreview.objects.get(profile=self.request.user.userprofile).cover.url
+        context['form'] = BookForm(book)
+        context['book'] = book
+        return context
