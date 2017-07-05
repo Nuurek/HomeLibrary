25fa35b345955122e31b389f4db47592e00449a2
Nuurek <nurskijakub@gmail.com>
2017-07-05 16:12:05 +0200

    Updated base settings with static root. Added Procfile. Fixed bug with bad app name.

--- home_library/settings/base.py
+++ home_library/settings/base.py
@@ -117,6 +117,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
