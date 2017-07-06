9ebd95c9376d2b0189047ed17fdea6a15e6abf4f
Nuurek <nurskijakub@gmail.com>
2017-07-06 09:30:23 +0200

    Changed boto version to 3.

--- home_library/settings/production.py
+++ home_library/settings/production.py
@@ -22,4 +22,4 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 STATIC_URL = "https://{}/".format(AWS_S3_CUSTOM_DOMAIN)
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
